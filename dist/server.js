cc1decb453fdaa41f662dc6fbc9e166bd0c0a62f
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-03-11 16:41:26 -0300

    feat: update dist build

--- dist/server.js
+++ dist/server.js
@@ -9,7 +9,10 @@ const routes_1 = require("./routes");
 const express_fileupload_1 = __importDefault(require("express-fileupload"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use((0, cors_1.default)());
+app.use((0, cors_1.default)({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 app.use((0, express_fileupload_1.default)({
     limits: { fileSize: 50 * 1024 * 1024 }
 }));
