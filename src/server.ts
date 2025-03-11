756acdb8201b2f1fd46381b012eb2e134228234f
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-03-11 16:41:03 -0300

    fix: update problem cors

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,10 @@ import fileUpload from 'express-fileupload';
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 app.use(fileUpload({
     limits: {fileSize: 50 * 1024 * 1024}
 }))
