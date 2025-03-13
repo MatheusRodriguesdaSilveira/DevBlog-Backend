922e5b456e98f01a4185ab687696245370ef4089
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-03-13 14:12:51 -0300

    style: formats the code

--- src/services/like/LikePostService.ts
+++ src/services/like/LikePostService.ts
@@ -6,14 +6,14 @@ class LikeService {
       if (!postId || !userId) {
         throw new Error("Post ID e User ID são obrigatórios.");
       }
-  
+
       const like = await prismaClient.like.findFirst({
         where: {
           userId,
           postId,
         },
       });
-      
+
       if (like) {
         await prismaClient.like.delete({
           where: {
@@ -23,10 +23,10 @@ class LikeService {
             id: true,
           },
         });
-  
+
         return { message: "Like removido", id: like.id };
       }
-  
+
       const newLike = await prismaClient.like.create({
         data: {
           userId,
@@ -39,9 +39,7 @@ class LikeService {
           createdAt: true,
         },
       });
-      
-      console.log("Novo like criado:", newLike);
-      
+
       return {
         message: "Like adicionado",
         id: newLike.id,
@@ -49,12 +47,10 @@ class LikeService {
         userId: newLike.userId,
         createdAt: newLike.createdAt,
       };
-      
     } catch (error) {
       console.error("Erro no serviço de like:", error);
       throw new Error("Erro ao processar o like.");
     }
-    
   }
 
   async getLikesByPostId(post_id: string) {
@@ -69,39 +65,39 @@ class LikeService {
           select: {
             id: true,
             name: true,
-          }
+          },
         },
-        createdAt: true
-      }
+        createdAt: true,
+      },
     });
     return likes;
   }
 
-async getLikeIdsByUser(userId: string) {
-  try {
-    const user = await prismaClient.user.findUnique({
-      where: {
-        id: userId
-      }
-    });
-    if (user) {
-      const likes = await prismaClient.like.findMany({
+  async getLikeIdsByUser(userId: string) {
+    try {
+      const user = await prismaClient.user.findUnique({
         where: {
-          userId: userId, // Filtra pelo ID do usuário
-        },
-        include: {
-          post: true, // Inclui detalhes da postagem curtida
+          id: userId,
         },
       });
-      return likes;
-    } else {
-      throw new Error("User not found");
+      if (user) {
+        const likes = await prismaClient.like.findMany({
+          where: {
+            userId: userId, // Filtra pelo ID do usuário
+          },
+          include: {
+            post: true, // Inclui detalhes da postagem curtida
+          },
+        });
+        return likes;
+      } else {
+        throw new Error("User not found");
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
-  } catch (error) {
-    console.error(error);
-    throw error;
   }
-}
 
   async deleteLike(id: number) {
     await prismaClient.like.delete({
