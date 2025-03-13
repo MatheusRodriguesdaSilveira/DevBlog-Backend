ade3391bb9282370d75db03ea528b4eb763e5a54
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-03-13 14:13:34 -0300

    adds step by step instructions to delete a profile

--- src/services/user/DeleteUserService.ts
+++ src/services/user/DeleteUserService.ts
@@ -11,13 +11,38 @@ class DeleteUserService {
     });
 
     if (!userAlreadyExists) {
-      throw new Error("Email não encontrado");
+      throw new Error("Usuário não encontrado");
     }
 
+    // Esses sao os passos para deletar o user com suas foreign keys:
+    // 1
+    await prismaClient.comment.deleteMany({
+      where: { post: { userId: user_id } },
+    });
+
+    // 2
+    await prismaClient.like.deleteMany({
+      where: { post: { userId: user_id } },
+    });
+
+    // 3
+    await prismaClient.post.deleteMany({ where: { userId: user_id } });
+
+    // 4
+    await prismaClient.like.deleteMany({ where: { userId: user_id } });
+
+    // 5
+    await prismaClient.comment.deleteMany({ where: { userId: user_id } });
+
+    // 6
+    await prismaClient.follower.deleteMany({ where: { followerId: user_id } });
+    await prismaClient.follower.deleteMany({ where: { followedId: user_id } });
+
+    // 7
+    await prismaClient.userToken.deleteMany({ where: { userId: user_id } });
+
     const user = await prismaClient.user.delete({
-      where: {
-        id: user_id,
-      },
+      where: { id: user_id },
     });
 
     return user;
