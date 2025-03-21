dac2f48afa41cb85d4530e41c12d86946f5d8e79
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-03-21 17:20:38 -0300

    feat: add follow methods

--- src/services/user/GetUserByIdService.ts
+++ src/services/user/GetUserByIdService.ts
@@ -1,42 +1,44 @@
 import { prismaClient } from "../../prisma";
 
 class GetUserByIdService {
-    async execute(user_id: string) {
-        const user = await prismaClient.user.findUnique({
-            where: {
-                id: user_id
-            },
-            select: {
-                name: true,
-                id: true,
-                descriptionProfile: true,
-                blogProfile: true,
-                linkedinProfile: true,
-                profilePicture: true,
-                posts: {
-                    select: {
-                        id: true,
-                        title: true,
-                        description: true,
-                        imageUrl: true,
-                        comments: {
-                            select: {
-                                content: true,
-                                user: {
-                                    select: {
-                                        name: true,
-                                        profilePicture: true,
-                                    }
-                                },
-                            },
-                        },
-                    },
+  async execute(user_id: string) {
+    const user = await prismaClient.user.findUnique({
+      where: {
+        id: user_id,
+      },
+      select: {
+        name: true,
+        id: true,
+        descriptionProfile: true,
+        blogProfile: true,
+        linkedinProfile: true,
+        profilePicture: true,
+        followers: true,
+        following: true,
+        posts: {
+          select: {
+            id: true,
+            title: true,
+            description: true,
+            imageUrl: true,
+            comments: {
+              select: {
+                content: true,
+                user: {
+                  select: {
+                    name: true,
+                    profilePicture: true,
+                  },
                 },
-            }
-        });
+              },
+            },
+          },
+        },
+      },
+    });
 
-        return user;
-    }
+    return user;
+  }
 }
 
 export { GetUserByIdService };
