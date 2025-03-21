07b21904d2ddc9256f17efe7e75e0a3c3b1a0ca5
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-03-21 17:18:55 -0300

    feat: add new follow controller

---
+++ src/controllers/follower/FollowerController.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express";
+import { FollowerService } from "../../services/follower/FollowerService";
+
+class FollowerController {
+  async handle(req: Request, res: Response) {
+    const { followerId, followedId } = req.body;
+    try {
+      const followerService = new FollowerService();
+      const follow = await followerService.execute(followerId, followedId);
+      return res.json(follow);
+    } catch (error) {
+      console.error("Erro ao seguir/deixar de seguir usuário:", error);
+      return res
+        .status(500)
+        .json({ error: "Erro ao processar a ação de seguir." });
+    }
+  }
+
+  async deleteFollow(req: Request, res: Response) {
+    const { followerId, followedId } = req.body;
+    try {
+      const followerService = new FollowerService();
+      const follow = await followerService.deleteFollow(followerId, followedId);
+      return res.json(follow);
+    } catch (error) {
+      console.error("Erro ao seguir/deixar de seguir usuário:", error);
+      return res
+        .status(500)
+        .json({ error: "Erro ao processar a ação de seguir." });
+    }
+  }
+}
+
+export { FollowerController };
