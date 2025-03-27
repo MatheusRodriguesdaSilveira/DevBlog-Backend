cf08ee57eecc6a32b874b7afb66c1eb5497d91fa
MatheusRodriguesdaSilveira <matheussilveira2005@gmail.com>
2025-03-27 01:32:02 -0300

    feat: add new GetUnfollowedUser controller

---
+++ src/controllers/follower/GetUnfollowedUserController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from "express";
+import { GetUnfollowedUsersService } from "../../services/follower/GetUnfollowedUserService";
+
+class GetUnfollowedUsersController {
+  async handle(req: Request, res: Response) {
+    const loggedUserId = req.user_id; // Pegando do middleware de autenticação
+
+    const service = new GetUnfollowedUsersService();
+    const users = await service.execute(loggedUserId);
+
+    return res.json(users);
+  }
+}
+
+export { GetUnfollowedUsersController };
